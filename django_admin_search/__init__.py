327dff825fef1d49e69417d1369febcb16139cb4
shinneider-ivoryit <shinneider@ivoryit.com.br>
2018-08-27 18:22:11 -0300

    Translate pt-br files

---
+++ django_admin_search/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+default_app_config = 'django_admin_search.apps.DjangoAdminSearchConfig'
---
+++ django_admin_search/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+from django.utils.translation import ugettext as _
+
+
+class DjangoAdminSearchConfig(AppConfig): # Our app config class
+    name = 'django_admin_search'
+    verbose_name = _('Django Admin Search')

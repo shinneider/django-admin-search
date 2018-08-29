35cea6957331f3e4b1f57cbdf249b5d3d9c279b8
shinneider-ivoryit <shinneider@ivoryit.com.br>
2018-08-29 11:44:18 -0300

    Translate pt-br files to package in pipy

--- setup.py
+++ setup.py
@@ -14,7 +14,11 @@ setup(
     author_email='shinneider-libanio@hotmail.com',
     url='https://github.com/shinneider/django_admin_search',
     license='MIT',
-    packages=find_packages() + ['django_admin_search/templates', 'django_admin_search/static'],
+    packages=find_packages() + [
+        'django_admin_search/templates', 
+        'django_admin_search/static',
+        'django_admin_search/locale'
+    ],
     install_requires=[
         # 'Django>=2.0',
         # 'Python>=3.5',

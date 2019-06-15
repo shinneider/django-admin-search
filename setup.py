2394f0d4fce8abfe2d3742ce1d3d0ffcbf910e08
shinneider <shinneider-libanio@hotmail.com>
2019-06-15 19:48:12 -0300

    README corrections for PyPi

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 from io import open
-from setuptools import find_packages, setup
-from django_admin_search.meta import VERSION
 
+from setuptools import find_packages, setup
+
+from django_admin_search.meta import VERSION
 
 setup(
     name='django-admin-search',
     version=str(VERSION),
     description='The "Django Admin Search" is a advanced search modal for django admin',
     long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     author='Shinneider Libanio da Silva',
     author_email='shinneider-libanio@hotmail.com',
     url='https://github.com/shinneider/django_admin_search',
     license='MIT',
     packages=find_packages() + [
-        'django_admin_search/templates', 
+        'django_admin_search/templates',
         'django_admin_search/static',
         'django_admin_search/locale'
     ],

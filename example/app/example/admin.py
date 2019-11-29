0a6243d79e4be3819bf0b75d193887f014a8e9d4
shinneider <shinneider-libanio@hotmail.com>
2019-11-29 14:57:27 -0300

    add setup classifiers

--- example/app/example/admin.py
+++ example/app/example/admin.py
@@ -11,6 +11,7 @@ from .models import Area
 class AreaAdmin(AdvacedSearchAdmin):
     form = AreaForm
     search_form = AreaSearchForm
+    # list_per_page = 1
 
     def search_description(self, field, field_value, form_field, request, param_values):
         """

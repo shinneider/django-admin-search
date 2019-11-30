b248758b54ffa472009b9c60b75b25cc74f4b3a0
shinneider <shinneider-libanio@hotmail.com>
2019-11-30 00:29:59 -0300

    clean code

--- django_admin_search/admin.py
+++ django_admin_search/admin.py
@@ -6,16 +6,6 @@ from django.db.models import Q
 
 from django_admin_search import utils as u
 
-class AdvancedSearchChangeList(ChangeList):
-    def __init__(self, request, *args, **kwargs):
-        super().__init__(request, *args, **kwargs)
-        self.full_params = None
-
-    def get_query_string(self, *args, **kwargs):
-        self.params, self.full_params = self.full_params, self.params
-        result = super().get_query_string(*args, **kwargs)
-        self.params, self.full_params = self.full_params, self.params
-        return result
 
 class AdvancedSearchAdmin(ModelAdmin):
     """
@@ -24,71 +14,47 @@ class AdvancedSearchAdmin(ModelAdmin):
     change_list_template = 'admin/custom_change_list.html'
     advanced_search_fields = {}
 
-    def get_changelist(self, request):
-        return AdvancedSearchChangeList
-
-    def get_changelist_instance(self, request):
-        """
-        prevent error by removing advanced search params from GET params
-        """
-        get_copy = request.GET.copy()
-        request.GET._mutable = True
-        self.extract_advanced_search_terms(request.GET)
-        request.GET._mutable = False
-        r = super().get_changelist_instance(request)
-        r.full_params = dict(get_copy.items())
-        request.GET = get_copy
-        return r
-
-    def lookup_allowed(self, lookup, value):
-        """
-            override django admin 'lookup_allowed'
-        """
-        if lookup in self.advanced_search_form().fields.keys():
-            return True
-        return super().lookup_allowed(lookup, value)
-
     def get_queryset(self, request):
         """
             override django admin 'get_queryset'
         """
         qs = super().get_queryset(request)
         return self.advanced_search(request, qs)
+        
+    def get_changelist_instance(self, request):
+        """
+        prevent error by removing advanced search params from GET params
+        """
+        request.GET._mutable = True
+        self.extract_advanced_search_terms(request.GET)
+        request.GET._mutable = False
+        return super().get_changelist_instance(request)
 
     def changelist_view(self, request, extra_context=None):
-        advanced_search_form = self.extract_advanced_search_terms(dict(request.GET.items()))
-        extra_context = {'asf': advanced_search_form}
+        self.search_form_data = self.search_form(request.GET)
+        extra_context = {'asf': self.search_form_data}
         return super().changelist_view(request, extra_context=extra_context)
 
-    def advanced_search(self, request, qs):
-        qs = qs.filter(self.advanced_search_query(request, Q(), self.advanced_search_fields))
-        self.advanced_search_fields = {}  # Clear CACHED Get values
-        return qs
-
-    def advanced_search_form(self, request=None):
-        if hasattr(self, 'search_form'):
-            return self.search_form(data=request)
-
-        return None
-    
     def extract_advanced_search_terms(self, param_dict):
-        advanced_search_form = self.advanced_search_form(param_dict)
-        if advanced_search_form is not None:
-            for key in advanced_search_form.fields.keys():
+        if self.search_form_data is not None:
+            for key in self.search_form_data.fields.keys():
                 temp = param_dict.pop(key, None)
                 if temp:  # there is a field but it's empty so it's useless
                     self.advanced_search_fields[key] = temp
-        return advanced_search_form
-
+                    
+    def advanced_search(self, request, qs):
+        qs = qs.filter(self.advanced_search_query(request, Q(), self.advanced_search_fields))
+        return qs
+    
     def advanced_search_query(self, request, query, param_values):
         """
             Get form and mount filter query if form is not none
         """
-        form = self.advanced_search_form()
+        form = self.search_form_data
         if form is None:
             return query
 
-        for field, form_field in self.advanced_search_form().fields.items():
+        for field, form_field in self.search_form_data.fields.items():
             field_value = param_values[field][0] if field in param_values else None
 
             # to overide default filter for a sigle field

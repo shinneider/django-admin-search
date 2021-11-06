9ca70d6297f2979f7f9102c6a6178f0ba8766d11
Shinneider L. Silva <shinneider-libanio@hotmail.com>
2021-11-06 17:01:15 -0300

    bump to 0.3.10

--- django_admin_search/__init__.py
+++ django_admin_search/__init__.py
@@ -4,7 +4,7 @@ try:
 except ImportError:
     django = None
 
-__version__ = '0.3.9'
+__version__ = '0.3.10'
 
 if django and django.VERSION < (3, 2):  # pragma: no cover
-    default_app_config = 'django_admin_inline_paginator.apps.DjangoAdminInlinePaginatorConfig'
+    default_app_config = 'django_admin_search.apps.DjangoAdminSearchConfig'

4501b77f75639f964aa4272d05c54b23d9a7f78e
Shinneider L. Silva <shinneider-libanio@hotmail.com>
2022-08-24 01:50:34 -0300

    Bump to v0.3.12

--- django_admin_search/__init__.py
+++ django_admin_search/__init__.py
@@ -4,7 +4,7 @@ try:
 except ImportError:
     django = None
 
-__version__ = '0.3.11'
+__version__ = '0.3.12'
 
 if django and django.VERSION < (3, 2):  # pragma: no cover
     default_app_config = 'django_admin_search.apps.DjangoAdminSearchConfig'  # pylint: disable=invalid-name
